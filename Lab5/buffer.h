b60fb20278a05e92f4849fc39a4368a211013ca8
ZkunCry <aksentev.03@mail.ru>
2024-02-09 23:59:22 +0500

    init lab5

---
+++ Lab5/buffer.h
@@ -0,0 +1,15 @@
+#ifndef bufferH 
+#define bufferH 
+#include <iostream> 
+#include <windows.h> 
+using namespace std;
+class Buffer {
+protected:
+	Buffer() {}
+static Buffer* Buf; public:
+	static Buffer* CreateBuffer(int);
+	virtual int GetItem(void) = 0;
+	virtual void PutItem(int) = 0;
+	virtual ~Buffer() {}
+};
+#endif
