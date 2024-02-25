c43e0a1fb444f35d8f5897aae6738bd885352f4e
ZkunCry <aksentev.03@mail.ru>
2024-02-25 20:49:38 +0500

    change clientside code

--- Lab6/Client.cpp
+++ Lab6/Client.cpp
@@ -2,8 +2,28 @@
 #include <string>
 #include <cstring>
 #include <WS2tcpip.h>
+#include <thread>
+#include <mutex>
 #pragma comment(lib, "ws2_32.lib")
+std::mutex mut;
+bool isDisconnectOrErrors = false;
 
+void receiveMessages(SOCKET sock) {
+    char buffer[1024];
+    while (true) {
+        int bytesReceived = recv(sock, buffer, sizeof(buffer), 0);
+        if (bytesReceived == 0) {
+            isDisconnectOrErrors = true;
+            std::cout << "Server closed the connection. Quitting..." << std::endl;
+            break;
+        }
+        else if (bytesReceived == SOCKET_ERROR) {
+            isDisconnectOrErrors = true;
+            std::cerr << "Error in receiving data from server. Quitting..." << std::endl;
+            break;
+        }
+    }
+}
 int main() {
     //Инициализация WinSock
     WSADATA wsData;
@@ -42,12 +62,22 @@ int main() {
     std::getline(std::cin, name);
     send(sock, name.c_str(), name.size(), 0);
     std::string message;
-
+    /*Запуск обработчика событий.
+       Выставляет флаг isDisconnectedOrErrors в true, если произошла ошибка со стороны сервера
+       Запускается в отдельном потоке для того, чтобы не блокировался основной поток.
+    */
+    std::thread receiver(receiveMessages, sock);
     char buffer[1024];
-    while (message !="exit") {
+    while (message !="exit" && !isDisconnectOrErrors) {
+        if (isDisconnectOrErrors)
+            break;
+        std::cout << "Enter your message: ";
         std::getline(std::cin, message);
         send(sock, message.c_str(), message.size(), 0);
+  
     }
+    //Ожидание
+    receiver.join();
     std::cout << "Disconnecting" << std::endl;
     closesocket(sock);
     WSACleanup();
